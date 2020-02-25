30b5d16cac2934ed5919124805b0aac716badf29
Yating(Esther) Su <yaringsu@foxmail.com>
2020-02-25 16:12:02 -0500

    Updates

---
+++ precache-manifest.4f990f084ce6ff4196f57ab26e423582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6561288384f010d686b34d09aca356de",
+    "url": "/Interview-Test/index.html"
+  },
+  {
+    "revision": "d97e5deb591d246b797a",
+    "url": "/Interview-Test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "463c850b94c6724acc4f",
+    "url": "/Interview-Test/static/js/2.4f64011e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Interview-Test/static/js/2.4f64011e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97e5deb591d246b797a",
+    "url": "/Interview-Test/static/js/main.29c83753.chunk.js"
+  },
+  {
+    "revision": "88945447f5d1121aa663",
+    "url": "/Interview-Test/static/js/runtime-main.04921c07.js"
+  }
+]);
